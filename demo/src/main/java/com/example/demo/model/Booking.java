67217239d902580122ed1ef5a078168b1302f98f
anastasija-mickoska <anamicko1@gmail.com>
2025-01-24 21:29:56 +0100

    first commit

---
+++ demo/src/main/java/com/example/demo/model/Booking.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "discount_id", nullable = false)
+    private Discount discount;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date reservationDate;
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Discount getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Discount discount) {
+        this.discount = discount;
+    }
+
+    public Date getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(Date reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+}
